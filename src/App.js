2eafb0300c05e4908122208243127673a3fc3eec
Sevastian Sergiu <sevastian.sergiu@terranet.md>
2019-09-24 10:35:13 +0300

    routing features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const TestPage = () => (
+  <div>
+    <h1>Test</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/test" component={TestPage} />
+      </Switch>
     </div>
   );
 }
