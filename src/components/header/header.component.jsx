5defb6939f5b77bcbef04cf6339f22fd84d26bb6
Sevastian Sergiu <sevastian.sergiu@terranet.md>
2019-11-13 12:58:08 +0200

    adding header component and links to pages

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/logo";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 import "./header.styles.scss";
 
 const Header = () => (
   <div className="header">
-    <Link to="/">
+    <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
     <div className="options">
